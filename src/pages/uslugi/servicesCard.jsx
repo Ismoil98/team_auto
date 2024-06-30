5202dd07b54185531f5531ac6f769b38e45cc102
Steelgrou <steelgrou03@gmail.com>
2024-06-30 23:34:35 +0500

    services page first commit

---
+++ src/pages/uslugi/servicesCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function servicesCard() {
+    return (
+        <div>
+            <h2>Services</h2>
+            <div className="services-wrapper">
+                <div className="first-card">
+                    <img src="" alt="" />
+                    <h3 className='first-card__title'>Sports Car Rental Dubai Style Tour in Dubai</h3>
+                    <h4 className='first-card__subtitle'>Experience the thrill of a dune buggy tour in Dubai with us. We offer free hotel pick-up & drop-off service. Book now!</h4>
+                    <a href=""><span>LEARN MORE</span> <img src="https://autozoomrental.com/static/media/cardLink.bed99f62ffc95c7f584de45ef8c56c7e.svg" alt="" /></a>
+                </div>
+                <div className="second-card"></div>
+            </div>
+        </div>
+    )
+}
