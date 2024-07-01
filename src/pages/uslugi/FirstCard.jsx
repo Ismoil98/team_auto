ef7bb1828cbfe20ca405173f211df1f5436dced6
Steelgrou <steelgrou03@gmail.com>
2024-07-01 12:53:29 +0500

    services page second  commit

---
+++ src/pages/uslugi/FirstCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function FirstCard() {
+    return (
+        <div>
+            <p>
+                Sports Car Rental Dubai Style Tour in Dubai
+            </p>
+
+
+        </div>
+    )
+}
