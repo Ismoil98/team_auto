fac779261a9b7a9d507c7af6c3d5daba0f1d175f
Ismoil98 <ismoil6366>
2024-07-01 19:37:05 +0500

    starter

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import {Suspense} from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import {Home, Blogs, CarsAbout, CarsPage, Contacts, Faq, Terms, Uslugi, UslugiFirst, UslugiSecond, AboutUs} from './pages/index'
+import PageFirst from './pages/blogs/BlogInnerPage/PageFirst/PageFirst'
+import PageSecond from './pages/blogs/BlogInnerPage/PageSecond/PageSecond'
+import PageThird from './pages/blogs/BlogInnerPage/PageThird/PageThird'
 import Loader from './components/loader/Loader'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
@@ -97,6 +100,30 @@ const App = () => {
           element={
             <Suspense fallback={<Loader/>}>
               <AboutUs/>
+            </Suspense>
+          }
+          />
+          <Route 
+          path='/firstPage'
+          element={
+            <Suspense fallback={<Loader/>}>
+              <PageFirst/>
+            </Suspense>
+          }
+          />
+          <Route 
+          path='/secondBlogPage'
+          element={
+            <Suspense fallback={<Loader/>}>
+              <PageSecond/>
+            </Suspense>
+          }
+          />
+          <Route 
+          path='/thirdBlogPage'
+          element={
+            <Suspense fallback={<Loader/>}>
+              <PageThird/>
             </Suspense>
           }
           />
